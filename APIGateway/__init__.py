a1640f133f0697c578de3f675bb1a72e44dde8fe
George Vragalev <78079918+GeorgeVragalev@users.noreply.github.com>
2023-09-17 01:35:54 -0700

    added docker and kubernetes support

--- APIGateway/__init__.py
+++ APIGateway/__init__.py
@@ -3,55 +3,26 @@ import requests
 import redis
 from threading import Semaphore
 from hashlib import sha256
-from consul import Consul
 
 app = Flask(__name__)
 
 # Initialize Redis
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(host='localhost', port=6379, db=0)  # Assume Redis is running as 'redis-service' in Kubernetes
 
 # Initialize Semaphore for limiting concurrency
-sem = Semaphore(10)  # Allow 10 concurrent tasks at max
+sem = Semaphore(10)
 
-# Initialize Consul client
-consul = Consul(host='localhost', port=8500)
-
-def get_service_url(service_name):
-    index, data = consul.catalog.service(service_name)
-    if data:
-        service = data[0]
-        return f'http://{service["ServiceAddress"]}:{service["ServicePort"]}'
-    else:
-        return None
 
 def cache_key(action, payload):
     """Generate a cache key by hashing the action and payload."""
     return sha256(f"{action}{str(payload)}".encode()).hexdigest()
 
-@app.route('/user/<action>', methods=['POST'])
-def user_service(action):
-    service_url = get_service_url('user-service')
-    if not service_url:
-        return 'Service not found', 404
 
-    response = requests.post(f'{service_url}/{action}', json=request.json)
-    return (response.text, response.status_code, response.headers.items())
+@app.route('/api/<service>/<action>', methods=['GET', 'POST'])
+def generic_service(service, action):
+    service_url = f"http://{service}-service:8080"  # Use DNS resolution in Kubernetes
 
-@app.route('/trailer/<action>', methods=['POST'])
-def trailer_service(action):
-    auth_key = request.headers.get("Authorization")
-
-    user_service_url = get_service_url('user-service')
-    if not user_service_url:
-        return 'User Service not found', 404
-
-    # Validate the key with User service
-    auth_response = requests.post(f'{user_service_url}/validate', headers={'Authorization': auth_key})
-    if auth_response.status_code != 200:
-        return 'Unauthorized', 401
-
-    # Generate cache key
-    key = cache_key(action, request.json)
+    key = cache_key(action, request.json if request.method == 'POST' else request.args)
 
     # Try fetching the result from cache
     cached_result = r.get(key)
@@ -59,29 +30,17 @@ def trailer_service(action):
         return jsonify({"result": cached_result.decode("utf-8"), "source": "cache"})
 
     with sem:
-        task_id = f"task:{action}"
-
-        # Check if task is within concurrency limit and timeouts
-        if r.exists(task_id):
-            return 'Too Many Requests', 429
-
-        r.setex(task_id, 5, 'running')  # 5-second timeout
-
-        trailer_service_url = get_service_url('trailer-service')
-        if not trailer_service_url:
-            return 'Trailer Service not found', 404
-
-        # Forward the request to the Trailer service
-        response = requests.post(f'{trailer_service_url}/{action}', json=request.json)
+        if request.method == 'GET':
+            response = requests.get(f'{service_url}/{action}', params=request.args)
+        else:  # POST
+            response = requests.post(f'{service_url}/{action}', json=request.json)
 
         if response.status_code == 200:
             # Cache the result with a TTL of 60 seconds
             r.setex(key, 60, response.text)
 
-        # Remove the task from Redis to signal completion
-        r.delete(task_id)
-
     return (response.text, response.status_code, response.headers.items())
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
