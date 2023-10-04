a8083a56d3b75f0a7ef24de6fa07787e5b589c17
George Vragalev <78079918+GeorgeVragalev@users.noreply.github.com>
2023-10-04 14:34:33 +0300

    added circuit breaker in api gateway

--- APIGateway/app.py
+++ APIGateway/app.py
@@ -4,8 +4,10 @@ import redis
 from threading import Semaphore
 from hashlib import sha256
 import logging
+import pybreaker
 from requests.exceptions import Timeout
 
+# Initialize Flask app
 app = Flask(__name__)
 
 # Initialize Redis
@@ -13,6 +15,9 @@ r = redis.Redis(host='host.docker.internal', port=6379, db=0)
 
 # Initialize Semaphore for limiting concurrency
 sem = Semaphore(10)
+
+# Initialize Circuit Breaker
+breaker = pybreaker.CircuitBreaker(fail_max=3, reset_timeout=60)
 
 # Service registry for local service discovery
 service_registry = {
@@ -23,16 +28,19 @@ service_registry = {
 def discover_service(service_name):
     return service_registry.get(service_name, None)
 
-
 def cache_key(action, payload):
-    """Generate a cache key by hashing the action and payload."""
     return sha256(f"{action}{str(payload)}".encode()).hexdigest()
-
 
 @app.route('/status')
 def index():
     return 'Api gateway working on port: 5000'
 
+@breaker
+def perform_request(url, method, params=None, json=None):
+    if method == 'GET':
+        return requests.get(url, params=params, timeout=5)
+    else:
+        return requests.post(url, json=json, timeout=5)
 
 @app.route('/api/<service>/<action>', methods=['GET', 'POST'])
 def generic_service(service, action):
@@ -43,34 +51,30 @@ def generic_service(service, action):
             return jsonify({"error": "Service not found"}), 404
 
         key = cache_key(action, request.json if request.method == 'POST' else request.args)
-
-        # Try fetching the result from cache
         cached_result = r.get(key)
         if cached_result:
             return jsonify({"result": cached_result.decode("utf-8"), "source": "cache"})
 
         with sem:
-            if request.method == 'GET':
-                response = requests.get(f'{service_url}/{action}', params=request.args, timeout=5)
-            else:
-                response = requests.post(f'{service_url}/{action}', json=request.json, timeout=5)
-
+            response = perform_request(f'{service_url}/{action}', request.method, params=request.args, json=request.json)
             if response.status_code == 200:
                 r.setex(key, 60, response.text)
+                return response.text, response.content
             else:
                 logging.error(f'Bad response from service: {response.status_code}, {response.text}')
                 return jsonify({"error": f"Bad response from service: {response.status_code}"}), response.status_code
-
-        return response.text, response.content
 
     except Timeout:
         logging.error(f'Timeout occurred while accessing {service}/{action}')
         return jsonify({"error": "Request timed out"}), 408
 
+    except pybreaker.CircuitBreakerError:
+        logging.error('Circuit breaker is open')
+        return jsonify({"error": "Circuit is open; failing fast"}), 503
+
     except Exception as e:
         logging.error(f'An error occurred: {str(e)}')
         return jsonify({"error": "An unexpected error occurred"}), 500
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
