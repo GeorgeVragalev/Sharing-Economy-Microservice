2e46b2345c1c2f32220874ef115cbed578f3fa64
George Vragalev <78079918+GeorgeVragalev@users.noreply.github.com>
2023-10-05 08:48:53 -0700

    Update app.py

--- APIGateway/app.py
+++ APIGateway/app.py
@@ -42,7 +42,7 @@ def cache_key(action, payload):
 
 @app.route('/status')
 def status():
-    return 'Api gateway working on port: 5000'
+    return 'API gateway working on port: 5000'
 
 
 @app.route('/clear_cache')
